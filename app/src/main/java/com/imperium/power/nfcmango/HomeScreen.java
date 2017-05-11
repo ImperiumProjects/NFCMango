15a4221bc2d6226d507fecc6d1e277c9f8447b2e
Jason Power <power@embl.de>
2017-05-11 15:27:19 +0200

    Added second view (NFC Screen)

--- app/src/main/java/com/imperium/power/nfcmango/HomeScreen.java
+++ app/src/main/java/com/imperium/power/nfcmango/HomeScreen.java
@@ -1,13 +1,23 @@
 package com.imperium.power.nfcmango;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class HomeScreen extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.imperium.power.nfcmango";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+    }
+
+    /** Called when the user taps the Catch! button */
+    public void tapCatch(View view) {
+        Intent intent = new Intent(this, NFCScreen.class);
+        startActivity(intent);
     }
 }
