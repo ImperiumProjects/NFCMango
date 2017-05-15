1ce2e270a4bfd0a7c758f6c668294b507e36a21d
ImperiumProjects <jasonjp2@hotmail.com>
2017-05-15 15:55:32 +0200

    Changed ListView implementation to use Fragments thus saving the Action Bar.

--- app/src/main/java/com/imperium/power/nfcmango/CaughtList.java
+++ app/src/main/java/com/imperium/power/nfcmango/CaughtList.java
@@ -1,29 +1,13 @@
 package com.imperium.power.nfcmango;
 
-import android.app.ListActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
 
-public class CaughtList extends ListActivity {
-
-    static final String[] PKMN =
-            new String[] { "Pikachu", "Vulpix", "Dragonite",
-                    "Bulbasaur", "Oddish", "Seadra"};
+public class CaughtList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_caught_list);
-        setListAdapter(new MobileArrayAdapter(this, PKMN));
-    }
-
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        //get selected items
-        String selectedValue = (String) getListAdapter().getItem(position);
-        Toast.makeText(this, selectedValue, Toast.LENGTH_SHORT).show();
-
+        setContentView(R.layout.activity_caught_list);
     }
 }
