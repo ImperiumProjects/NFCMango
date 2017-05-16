63344492aa38ad1bf2fee29d0f5fbc608cebcb4d
Jason Power <power@embl.de>
2017-05-16 11:18:34 +0200

    Updated list view with fragment and images.

--- app/src/main/java/com/imperium/power/nfcmango/PkmnListFragment.java
+++ app/src/main/java/com/imperium/power/nfcmango/PkmnListFragment.java
@@ -7,28 +7,84 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class PkmnListFragment extends ListFragment implements OnItemClickListener {
+
+    String[] names = new String[] {
+            "Pikachu",
+            "Bulbasaur",
+            "Dragonite",
+            "Seadra",
+            "Oddish",
+            "Vulpix"
+    };
+
+    int[] pokemon = new int[]{
+            R.drawable.pika,
+            R.drawable.bulb,
+            R.drawable.drag,
+            R.drawable.hors,
+            R.drawable.odd,
+            R.drawable.vulp
+    };
+
+    ArrayList<HashMap<String, String>> data = new ArrayList<HashMap<String,String>>();
+    SimpleAdapter adapter;
+
     @Override
-    public View onCreateView(LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.list_fragment, container, false);
-        return view;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        //MAP
+        HashMap<String, String> map=new HashMap<String, String>();
+        //FILL
+        for(int i=0;i<names.length;i++)
+        {
+            map=new HashMap<String, String>();
+            map.put("Player", names[i]);
+            map.put("Image", Integer.toString(pokemon[i]));
+            data.add(map);
+        }
+        //KEYS IN MAP
+        String[] from={"Player","Image"};
+        //IDS OF VIEWS
+        int[] to={R.id.nameTxt,R.id.imageView1};
+        //ADAPTER
+        adapter = new SimpleAdapter(getActivity(), data, R.layout.list_fragment, from, to);
+        setListAdapter(adapter);
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 
     @Override
+    public void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+        getListView().setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> av, View v, int pos,
+                                    long id) {
+                // TODO Auto-generated method stub
+                Toast.makeText(getActivity(), data.get(pos).get("Player"), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    /*@Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(),
                 R.array.PKMN, android.R.layout.simple_list_item_1);
         setListAdapter(adapter);
         getListView().setOnItemClickListener(this);
-    }
+    }*/
 
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position,long id) {
-        Toast.makeText(getActivity(), "Item: " + position, Toast.LENGTH_SHORT).show();
+    public void onItemClick(AdapterView<?> av, View v, int pos, long id) {
+        Toast.makeText(getActivity(), data.get(pos).get("Player"), Toast.LENGTH_SHORT).show();
     }
 }
