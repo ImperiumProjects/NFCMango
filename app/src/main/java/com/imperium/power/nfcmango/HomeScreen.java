c908290daca66b380da8f667f6df4b01ea26d795
ImperiumProjects <jasonjp2@hotmail.com>
2017-05-24 17:03:18 +0200

    Scrolling background on Home Screen view.

--- app/src/main/java/com/imperium/power/nfcmango/HomeScreen.java
+++ app/src/main/java/com/imperium/power/nfcmango/HomeScreen.java
@@ -1,9 +1,11 @@
 package com.imperium.power.nfcmango;
 
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 
 public class HomeScreen extends AppCompatActivity {
@@ -13,6 +15,25 @@ public class HomeScreen extends AppCompatActivity {
         try {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_home_screen);
+
+            final ImageView backgroundOne = (ImageView) findViewById(R.id.background_one);
+            final ImageView backgroundTwo = (ImageView) findViewById(R.id.background_two);
+
+            final ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
+            animator.setRepeatCount(ValueAnimator.INFINITE);
+            animator.setInterpolator(new LinearInterpolator());
+            animator.setDuration(10000L);
+            animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    final float progress = (float) animation.getAnimatedValue();
+                    final float width = backgroundOne.getWidth();
+                    final float translationX = width * progress;
+                    backgroundOne.setTranslationX(translationX);
+                    backgroundTwo.setTranslationX(translationX - width);
+                }
+            });
+            animator.start();
 
             ImageView pkball = (ImageView) findViewById(R.id.pokeballClick);
 
