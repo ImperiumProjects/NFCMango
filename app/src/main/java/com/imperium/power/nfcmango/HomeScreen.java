820b8541e954ebf07c70c465665c232d18c72c24
Jason Power <power@embl.de>
2017-06-29 16:39:19 +0200

    Removed HTTP POST request code

--- app/src/main/java/com/imperium/power/nfcmango/HomeScreen.java
+++ app/src/main/java/com/imperium/power/nfcmango/HomeScreen.java
@@ -4,24 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.PasswordTransformationMethod;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.TimeZone;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.FormBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -43,11 +29,6 @@ public class HomeScreen extends AppCompatActivity {
             final EditText passwordField = (EditText) findViewById(R.id.pwField);
             final TextView error = (TextView) findViewById(R.id.error_text);
 
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            sdf.setTimeZone(TimeZone.getDefault());
-            final String currentDateAndTime = sdf.format(new Date());
-            final OkHttpClient client = new OkHttpClient();
-
             passwordField.setTransformationMethod(new PasswordTransformationMethod());
 
             pkball.setOnClickListener(new View.OnClickListener() {
@@ -57,14 +38,13 @@ public class HomeScreen extends AppCompatActivity {
                         password = passwordField.getText().toString();
                         ///
                         try {
-                            doGetRequest("http://labday01.embl.de/login.php");
                         }
-                        catch(IOException e){
+                        catch(Exception e){
                             e.printStackTrace();
                         }
                         Intent intent = new Intent(getApplicationContext(), NFCScreen.class);
                         startActivity(intent);
-                        //finish();
+                        finish();
                     }
                     else{
                         if(usernameField.getText().toString().equals("")){
@@ -77,36 +57,6 @@ public class HomeScreen extends AppCompatActivity {
                             error.setText(R.string.home_screen_error);
                         }
                     }
-
-                }
-                void doGetRequest(String url) throws IOException{
-                    RequestBody body = new FormBody.Builder()
-                            .add("name", username)
-                            .add("password", password)
-                            .add("start_time", currentDateAndTime)
-                            .build();
-                    Request request = new Request.Builder()
-                            .url(url)
-                            .post(body)
-                            .build();
-                    client.newCall(request)
-                            .enqueue(new Callback() {
-                                @Override
-                                public void onFailure(final Call call, IOException e){
-                                    //Error
-                                    runOnUiThread(new Runnable() {
-                                        @Override
-                                        public void run(){
-                                            //show error or whatever
-                                        }
-                                    });
-                                }
-                                @Override
-                                public void onResponse(Call call, final Response response) throws IOException{
-                                    String res = response.body().string();
-                                    Log.d("responsecode", res);
-                                }
-                            });
                 }
             });
         } catch (Exception e) {
